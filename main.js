43b712f726f4987b1f0f0b2a800dece34ddd2831
Carlos Rodrigues <carlos.rodrigues@ev-box.com>
2017-05-05 11:02:06 +0200

    Using top and css transitions

--- main.js
+++ main.js
@@ -1,68 +1,50 @@
 (function () {
-    document.addEventListener("DOMContentLoaded", function () {
-        document
-            .getElementById("accordion")
-            .addEventListener("click", function (event) {
-                var el = event.target;
 
-                if (el.getAttribute("role") === "tab") {
-
-                    accordionBody = el.nextElementSibling;
-
-                    if (accordionBody.getAttribute("aria-expanded") === "false") {
-                        play(el);
-                        accordionBody.setAttribute("aria-expanded", true);
-                    }
-
-                    else {
-                        reverse(el);
-
-                        setTimeout(function () {
-                            accordionBody.setAttribute("aria-expanded", false);
-                        }, 400);
-                    }
-
-                }
-            });
+    document.addEventListener('DOMContentLoaded', function () {
+        document.getElementById('accordion').addEventListener('click', function (event) {
+            slideY(event.target);
+        });
     });
 
-    function play(el) {
-        console.log(el);
+    function slideY(el) {
+        const duration = 300;
 
-        var parent = el.parentElement;
-        var elH = el.offsetHeight;
-        parent.style.cssText = `height:${elH}px;`;
+        let parent = undefined;
+        let accordionBody = undefined;
 
-// Get the content height
-        var accordionBody = el.nextElementSibling;
-        accordionBody.classList.add('preRender');
-        var accordionBodyH = accordionBody.offsetHeight;
-        accordionBody.classList.remove('preRender');
+        if (el && el.getAttribute('role') === 'tab') {
+            parent = el.parentNode;
+            accordionBody = el.nextElementSibling;
 
-//set parent height
-        accordionBody.style.cssText = `height:${accordionBodyH}px;display:block;position:relative;top:-${accordionBodyH}px`;
-        parent.style.cssText = `height:${elH}px;position:relative;`;
-        parent.style.cssText = `height:${elH + accordionBodyH}px;position:relative;`;
+            playAnimation(accordionBody.getAttribute('aria-expanded') === 'true');
 
-        setTimeout(function () {
-            accordionBody.style.top = 0;
-        }, 0);
+        }
 
-    }
+        function playAnimation(invert) {
+            let elH = el.offsetHeight;
 
-    function reverse(el) {
+            accordionBody.classList.add('preRender');
+            let accordionBodyH = accordionBody.offsetHeight - 3;
+            accordionBody.classList.remove('preRender');
 
-        var parent = el.parentElement;
-        var elH = el.offsetHeight;
-        parent.style.cssText = `height:${elH}px`;
+            if (!invert) {
+                accordionBody.setAttribute('aria-expanded', 'true');
+                accordionBody.style.cssText = `height:${accordionBodyH}px;display:block;position:relative;top:-${accordionBodyH}px`;
+                parent.style.cssText = `height:${elH}px;position:relative;`;
 
-// Get the content height
-        var accordionBody = el.nextElementSibling;
-        var accordionBodyH = accordionBody.offsetHeight;
+                setTimeout(function () {
+                    parent.style.cssText = `height:${elH + accordionBodyH}px;position:relative;`;
+                    accordionBody.style.top = 0;
+                }, 0);
+            } else {
+                parent.style.cssText = `height:${elH}px`;
+                accordionBody.style.top = `-${accordionBodyH}px`;
+                setTimeout(function () {
+                    accordionBody.setAttribute("aria-expanded", false);
+                }, duration);
+            }
 
-//set parent height
-        accordionBody.style.top = `-${accordionBodyH}px`;
-
+        }
     }
 
 })();
